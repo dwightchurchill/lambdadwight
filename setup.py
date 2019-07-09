21cf65932c94c642160c32330c0715f82f3dae34
dwightchurchill <dwight.a.churchill@gmail.com>
2019-07-08 20:08:20 -0400

    updated setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lambdatadwight',
+      version='0.1',
+      description='Simple package to check nulls in your Pandas DataFrame and split datetimes into separate columns!',
+      url='http://github.com/storborg/',
+      author='Dwight Churchill',
+      author_email='dwight.a.churchill@gmail.com',
+      license='MIT',
+      packages=['lambdatadwight'],
+      zip_safe=False)
