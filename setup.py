31f8470352365a033e95cab5e3719c4e1ce86701
dwightchurchill <dwight.a.churchill@gmail.com>
2019-07-09 11:23:42 -0400

    updated name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lamdadata",
+    name="lamdadatadwight",
     version="0.0.2",
     author="Dwight Churchill",
     author_email="dwight.a.churchill@gmail.com",
