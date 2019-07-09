736c26ad090a749756474ca39ddac7fe6b89968f
dwightchurchill <dwight.a.churchill@gmail.com>
2019-07-09 11:51:20 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lamdadatadwightc",
-    version="0.0.2",
+    version="0.0.3",
     author="Dwight Churchill",
     author_email="dwight.a.churchill@gmail.com",
     description="Simple package to check nulls in your Pandas DataFrame and split datetimes into separate columns!",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="http://github.com/dwightchurchill/lambdadwight/",
-    download_url = 'https://github.com/dwightchurchill/lamdadatadwightc/dist/lamdadatadwightc-0.0.2.tar.gz',
+    download_url = 'https://github.com/dwightchurchill/lamdadatadwightc/dist/lamdadatadwightc-0.0.3.tar.gz',
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
