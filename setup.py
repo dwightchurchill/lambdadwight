43cb18d1ad44193008c8bd776532ef1e98409ea3
dwightchurchill <dwight.a.churchill@gmail.com>
2019-07-09 11:18:44 -0400

    Updated setup and README

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='lambdatadwight',
-      version='0.2',
-      description='Simple package to check nulls in your Pandas DataFrame and split datetimes into separate columns!',
-      long_description='Simple package to check nulls in your Pandas DataFrame and split datetimes into separate columns!',
-      long_description_content_type="text/markdown",
-      url='http://github.com/dwightchurchill/lambdadwight/',
-      author='Dwight Churchill',
-      author_email='dwight.a.churchill@gmail.com',
-      license='MIT',
-      packages=['lambdatadwight'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lamdadata",
+    version="0.0.2",
+    author="Dwight Churchill",
+    author_email="dwight.a.churchill@gmail.com",
+    description="Simple package to check nulls in your Pandas DataFrame and split datetimes into separate columns!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/dwightchurchill/lambdadwight/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
